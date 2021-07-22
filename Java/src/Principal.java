4c8b53bf11a27453af0c76ec50a8dbe47df908d2
Camilo Barreiro <1845@holbertonschool.com>
2021-07-22 03:51:48 -0500

    Add change

---
+++ Java/src/Principal.java
@@ -0,0 +1,25 @@
+public class Principal {
+    public static void main(String[] args) {
+        //Estudiante diana = new Estudiante();
+        //diana.setNombre("Diana");
+        //diana.setAppelido("Aristizabal");
+        //diana.setEdad(19);
+        //diana.setCarrera("Ingenieria de Sistemas");
+
+        Estudiante diana = new Estudiante("12345","diana","Aristizabal");
+        System.out.println("El nombre del estudiante es: " + diana.getNombreCompleto());
+        System.out.println("Asistencias: " + diana.getAsistencias());
+        diana.asistirAClase();
+        System.out.println("Asistencias: " + diana.getAsistencias());
+        //System.out.println("Carrera: " + diana.getCarrera());
+        diana.agregarNota(1);
+        diana.agregarNota(2);
+        diana.agregarNota(3);
+        diana.agregarNota(4);
+        diana.agregarNota(5);
+        //diana.agregarNota(7);
+        float resultado = diana.calcularPromedio(true);
+        System.out.printf("El promedio de las notas es: " + resultado);
+
+    }
+}
