46c422f4efc7c85a8eefaf7346936faa6ff467a1
Camilo Barreiro <1845@holbertonschool.com>
2021-08-08 20:50:01 -0500

    Exercise

---
+++ Java/ExerciseOOP2/src/collection/Music.java
@@ -0,0 +1,16 @@
+package collection;
+
+public class Music {
+    public static void main(String[] args) {
+        Collection Disk1 = new Collection();
+        /* -------------------------------------*/
+        Singer Shakira = new Singer("Shakira","1234");
+        Disk Miami = new Disk("WAKAWAK",1000000);
+        Disk kuku = new Disk("gogo",10101010);
+        Shakira.addDisk(Miami);
+        Shakira.addDisk(kuku);
+
+        Disk1.getSingers().add(Shakira);
+        System.out.println(Disk1.searchSinger("Hola"));
+    }
+}
