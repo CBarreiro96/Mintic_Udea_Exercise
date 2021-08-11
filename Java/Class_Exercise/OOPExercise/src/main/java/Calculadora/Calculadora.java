2e2de2f0a5fda23d43010b0934babf1d2a21d863
Camilo Barreiro <1845@holbertonschool.com>
2021-08-11 11:22:29 -0500

    exercise

---
+++ Java/Class_Exercise/OOPExercise/src/main/java/Calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculadora;
+
+/**
+ *
+ * @author userx
+ */
+public class Calculadora {
+
+    static int sumar(int i, int i0) {
+        return 9;
+    }
+    
+}
